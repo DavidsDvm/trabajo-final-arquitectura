94a9b567b15fe48a584a6d06accee52e75f04b58
David Vargas <davids10.dvm@gmail.com>
2025-05-16 16:20:19 -0500

    Fix: base path for prod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? '/trabajo-final-arquitectura' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/trabajo-final-arquitectura' : '',
   /* config options here */
 };
 
