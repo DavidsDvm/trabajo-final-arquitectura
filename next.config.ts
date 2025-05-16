d26549b82418cd4721bc51c8085695e5b0b5f201
David Vargas <davids10.dvm@gmail.com>
2025-05-16 16:33:29 -0500

    Fix: problem with gh-pges

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/trabajo-final-arquitectura' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/trabajo-final-arquitectura' : '',
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
